4874f5e42ec3b1e8e1a9e6c16307a8bba5e6ba54
Ahmad Mostafa <33914189+ahmadmostafa10495@users.noreply.github.com>
2020-04-17 21:44:52 +0200

    Components Part1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 // import { Navbar } from 'reactstrap';
 import {Nav, Navbar,NavbarBrand, NavDropdown, MenuItem, Tabs, ButtonToolbar, Button, Table, ButtonGroup, Row, Col, Grid, Panel, FormGroup, FormControl} from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 // function App() {
@@ -28,12 +29,13 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
         </Navbar>
+        <Menu />
       </div>
 
       );
