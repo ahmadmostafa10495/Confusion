d4013cdb81f1b811558d52a87ac669349fe43374
Ahmad Mostafa <33914189+ahmadmostafa10495@users.noreply.github.com>
2020-04-20 21:51:26 +0200

    Updates

---
+++ precache-manifest.13c8623a0853098385eb00ed439157a6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "257866dcaa65d52a0b7be1e37b00cf70",
+    "url": "/Confusion/index.html"
+  },
+  {
+    "revision": "dc936e1d96f2c295b66d",
+    "url": "/Confusion/static/css/2.0bcebd59.chunk.css"
+  },
+  {
+    "revision": "caa666b05d836674158f",
+    "url": "/Confusion/static/css/main.e03eb87c.chunk.css"
+  },
+  {
+    "revision": "dc936e1d96f2c295b66d",
+    "url": "/Confusion/static/js/2.6706f640.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Confusion/static/js/2.6706f640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caa666b05d836674158f",
+    "url": "/Confusion/static/js/main.49cfa892.chunk.js"
+  },
+  {
+    "revision": "590545f88b827cadfae1",
+    "url": "/Confusion/static/js/runtime-main.63091b3d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Confusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Confusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Confusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Confusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Confusion/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
